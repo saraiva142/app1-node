565ac8648bdb182d30fcde1f4190f82cc26d79a4
saraiva142 <saraivajandrade@gmail.com>
2025-03-07 08:51:11 -0300

    Tipos de Dados JS

--- aula01/tipo_de_dados.js
+++ aula01/tipo_de_dados.js
@@ -1,1 +1,44 @@
-console.log("Aula 01 - tipo de dados");
+console.log("Aula 01 - Tipo de Dados em JS");
+
+// Tipos de Dados em JS
+// 1. Number 
+console.log("Numero: ", 1);
+console.log("Numero: ", 1.5);
+let num = 1;
+console.log("Numero: ", num);
+let num2 = 1.5;
+console.log("Numero: ", num2);   
+// 2. String
+console.log("String: ", "Olá Mundo");
+let str = "Olá Mundo";
+console.log("String: ", str);
+// 3. Boolean
+console.log("Boolean: ", true);
+let bool = true;
+console.log("Boolean: ", bool);
+// 4. Undefined
+console.log("Undefined: ", undefined);
+let und;
+console.log("Undefined: ", und);
+// 5. Null
+console.log("Null: ", null);
+let n = null;
+console.log("Null: ", n);
+// 6. Object
+console.log("Object: ", {});
+let obj = {};
+console.log("Object: ", obj);
+let obj2 = {
+    nome: "João",
+    idade: 20
+};
+console.log("Object: ", obj2);
+// 7. Symbol
+console.log("Symbol: ", Symbol());
+let sym = Symbol();
+console.log("Symbol: ", sym);
+// 8. Function
+console.log("Function: ", function() {});
+let func = function() {};
+console.log("Function: ", func);
+
