e0336f6e74c62a27e31b36230493583f1657ab89
saraiva142 <saraivajandrade@gmail.com>
2025-03-14 07:27:17 -0300

    Js funcionando

--- aula01/tipo_de_dados.js
+++ aula01/tipo_de_dados.js
@@ -1,51 +1,6 @@
-console.log("Aula 01 - Tipo de Dados em JS");
-
-//Rodar o docker :
-//>docker run -it -d --name lts-node -v "C:/Users/Elisio da Silva/Desktop/Coisas do João/Construção de Software/app1-node:/app1-node" node:alpine
-//>docker exec -it lts-node sh
-//>cd app1-node
-//>cd aula01
-//>node tipo_de_dados.js
-
-// Tipos de Dados em JS
-// 1. Number 
-console.log("Numero: ", 1);
-console.log("Numero: ", 1.5);
-let num = 1;
-console.log("Numero: ", num);
-let num2 = 1.5;
-console.log("Numero: ", num2);   
-// 2. String
-console.log("String: ", "Olá Mundo");
-let str = "Olá Mundo";
-console.log("String: ", str);
-// 3. Boolean
-console.log("Boolean: ", true);
-let bool = true;
-console.log("Boolean: ", bool);
-// 4. Undefined
-console.log("Undefined: ", undefined);
-let und;
-console.log("Undefined: ", und);
-// 5. Null
-console.log("Null: ", null);
-let n = null;
-console.log("Null: ", n);
-// 6. Object
-console.log("Object: ", {});
-let obj = {};
-console.log("Object: ", obj);
-let obj2 = {
-    nome: "João",
-    idade: 20
-};
-console.log("Object: ", obj2);
-// 7. Symbol
-console.log("Symbol: ", Symbol());
-let sym = Symbol();
-console.log("Symbol: ", sym);
-// 8. Function
-console.log("Function: ", function() {});
-let func = function() {};
-console.log("Function: ", func);
-
+let botao = document.getElementById("botao")
+function mostrarMensagem() {
+    let nome = document.getElementById("nome").value
+    document.getElementById("resposta").innerHTML = "Olá, " + nome + "!"
+}
+botao.addEventListener("click", mostrarMensagem)
