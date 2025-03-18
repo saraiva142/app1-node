c982fe60a1089b6ca51092436df6655bb4f245e4
saraiva142 <saraivajandrade@gmail.com>
2025-03-18 15:03:58 -0300

    Calculadora Logica

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-"# app1-node" 
+# app1-node
+
+
+Para fazer o merge:
+
+Supondo que: Estou numa branch, quero comitar o que eu fiz e depois fazer o merge com o main
+
+Passos:
+* git add .
+* git commit -m "Commit..."
+* git push origin <branch>
+
+* git checkout main
+atualizar a main:
+* git pull origin main 
+* git merge <branch-que-queira-fazer-o-merge>
+* git push origin main
--- aula01/aula 02/lab02.js
+++ aula01/aula 02/lab02.js
@@ -3,6 +3,10 @@ let valor2 = document.getElementById('num2');
 let opAritmetico = document.getElementById('opAritmetico');
 let resultado = document.getElementById('resultado');
 
+let opLogico1 = document.getElementById('op1');
+let opLogico2 = document.getElementById('op2');
+let opLogico = document.getElementById('opLogico');
+let resultadoLogico = document.getElementById('resultadoLog');
 
 function calculoAritmetico(operador, valor1, valor2) {
     switch (operador) {
@@ -27,4 +31,29 @@ valor2.addEventListener('input', function () {
 });
 opAritmetico.addEventListener('input', function () {
     resultado.innerHTML = calculoAritmetico(opAritmetico.value, Number(valor1.value), Number(valor2.value));
+});
+
+function calculoLogico(operador, valor1, valor2) {
+    switch (operador) {
+        case '&':
+            return valor1 & valor2;
+        case '|':
+            return valor1 | valor2;
+        default:
+            return 'Operação inválida';
+    }
+}
+
+
+opLogico1.addEventListener('input', function () {
+    resultadoLogico.textContent = calculoLogico(opLogico.value, opLogico1.value, opLogico2.value);
+    //resultadoLogico.textContent = resultadoLogico;
+});
+opLogico2.addEventListener('input', function () {
+    resultadoLogico.textContent = calculoLogico(opLogico.value, opLogico1.value, opLogico2.value);
+    //resultadoLogico.textContent = resultadoLogico;
+});
+opLogico.addEventListener('click', function () {
+    resultadoLogico.textContent = calculoLogico(opLogico.value, opLogico1.value, opLogico2.value);
+    //resultadoLogico.textContent = resultadoLogico;
 });
